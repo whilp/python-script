0b581121cf227049a6a70eb591897e692186edc7
Will Maier <willmaier@ml1.net>
2010-12-29 14:06:00 -0600

    Add docstring, license.

--- script.py
+++ script.py
@@ -1,3 +1,30 @@
+#!/usr/bin/env python
+"""This is what an ideal Python script might look like.
+
+It's written to have several important properties; namely:
+
+  * readability: follows :PEP:`8`.
+  * portability: uses portable components where necessary.
+  * testability: unit and functional tests that can be run without causing side
+    effects.
+  * configurability: command line configuration of logging and other details.
+"""
+__license__ = """\
+Copyright (c) 2010 Will Maier <willmaier@ml1.net>
+
+Permission to use, copy, modify, and distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.\
+"""
+
 import logging
 import optparse
 import sys
